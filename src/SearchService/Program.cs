5efd5dba6794bf18d8dc1a9d65fe30a077b87aed
Gteoharov <Gteoharov@users.noreply.github.com>
2024-03-01 10:10:00 +0200

    Adding rabbitmq bus services

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,15 +1,23 @@
+using MassTransit;
 using Polly;
 using Polly.Extensions.Http;
 using SearchService;
 using SearchService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+// Only for demo purposes, in a real-world scenario, you should use a class library which share between Auction and Search services
+
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
-
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
