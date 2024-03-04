6f7f7944439d4a15b0c0dcc92e2fa8af6191529f
Gteoharov <Gteoharov@users.noreply.github.com>
2024-03-04 13:07:27 +0200

    Adding update and delete consumers

--- src/AuctionService/Controllers/AuctionController.cs
+++ src/AuctionService/Controllers/AuctionController.cs
@@ -87,6 +87,8 @@ public class AuctionController : ControllerBase
         auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
         auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
 
+        await _publishEndpoint.Publish(_mapper.Map<AuctionUpdated>(auction));
+
         var result = await _context.SaveChangesAsync() > 0;
 
         if (result) return Ok();
@@ -102,8 +104,9 @@ public class AuctionController : ControllerBase
         if (auction == null) return NotFound();
 
         // TODO: check seller == username
-
         _context.Auctions.Remove(auction);
+
+        await _publishEndpoint.Publish<AuctionDeleted>(new { Id = auction.Id.ToString() });
 
         var result = await _context.SaveChangesAsync() > 0;
 
